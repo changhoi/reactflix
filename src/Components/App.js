d27d715e312b655a138f7f12020d85de54d8271f
changhoi <0417kch@naver.com>
2019-03-19 20:55:36 +0900

    project init and error point

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Router from "Components/Router";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Router />
+      </>
+    );
+  }
+}
+
+export default App;
